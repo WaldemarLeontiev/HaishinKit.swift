7b175e83f3bd5c8b067f05518e8aded3793b4869
Waldemar Leontiev <waldemar.leontiev@gmail.com>
2018-02-12 21:09:16 +0300

    Added protocol SampleBufferProcessor, added sampleBufferProcessor hook

--- Sources/Net/NetStream.swift
+++ Sources/Net/NetStream.swift
@@ -186,6 +186,12 @@ open class NetStream: NSObject {
             self.mixer.videoIO.unregisterEffect(effect)
         }
     }
+    
+    open var sampleBufferProcessor: SampleBufferProcessor? {
+        didSet {
+            mixer.videoIO.sampleBufferProcessor = sampleBufferProcessor
+        }
+    }
 
     open func dispose() {
         lockQueue.async {
